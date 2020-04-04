af64ae0c93235b30b19f018f83636101864c6271
Emanuel Kluge <mail@emanuel-kluge.de>
2020-04-04 19:59:50 +0200

    test: check event detail view

---
+++ src/components/app/__tests__/detail-deletion-success.tsx
@@ -0,0 +1,66 @@
+/* eslint @typescript-eslint/no-non-null-assertion: "off" */
+
+import React, { FunctionComponent } from 'react';
+import format from 'date-fns/format';
+import { render, fireEvent, act, wait } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { RouteProps } from 'react-router';
+
+jest.mock('../../../hooks/use-user', () => ({
+  useUser: () => ({ username: 'test' }),
+}));
+jest.mock('react-router-dom', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { createElement, Fragment, Children } = require('react');
+  const mod = jest.requireActual('react-router-dom');
+  const BrowserRouter: FunctionComponent = ({ children }) =>
+    createElement(Fragment, void 0, Children.only(children));
+  return { ...mod, BrowserRouter };
+});
+jest.mock('aws-amplify', () => {
+  const { graphqlOperation } = jest.requireActual('aws-amplify');
+  const item = {
+    id: '1',
+    title: 'My awesome event!',
+    start: new Date(),
+    end: new Date(new Date(Date.now() + 1000 * 60 * 60)),
+    description: 'Lorem ipsum dolor.',
+    creator: 'test',
+  };
+  const graphql = jest
+    .fn()
+    .mockResolvedValueOnce({ data: { getCalendarEntry: item } })
+    .mockResolvedValueOnce({ data: {} })
+    .mockResolvedValue({ data: { listCalendarEntrys: { items: [] } } });
+  const API = { graphql };
+  return { graphqlOperation, API };
+});
+
+window.confirm = () => true;
+
+it('deletes the entry', async () => {
+  /* eslint-disable-next-line @typescript-eslint/no-var-requires */
+  const { App } = require('..');
+  let location: RouteProps['location'] | undefined;
+  const date = new Date();
+  const month = format(date, 'yyyy-MM');
+  const app = await render(
+    <MemoryRouter initialEntries={['/detail/1/']}>
+      <App />
+      <Route
+        path="*"
+        render={({ location: reactRouterLocation }) => {
+          location = reactRouterLocation;
+          return null;
+        }}
+      />
+    </MemoryRouter>
+  );
+  await wait();
+  const btn = await app.getByText('Termin löschen');
+
+  act(() => void fireEvent.click(btn));
+  await wait();
+
+  expect(location!.pathname).toBe(`/calendar/${month}/`);
+});
